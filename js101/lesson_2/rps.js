8e50e2a9476d0226cdfd8cdf83c7ccaae7ea10ff
Grant Ragsdale <gragsdale1988@gmail.com>
2024-01-20 10:34:16 -0600

    updated rps.js with TA suggestions

--- js101/lesson_2/rps.js
+++ js101/lesson_2/rps.js
@@ -32,42 +32,22 @@ prompt('Welcome to Rock, Paper, Scissors, Lizard, Spock!\n=> The first to 3 is t
 
 let userWinCounter = 0;
 let computerWinCounter = 0;
+let playAgainAnswer = 'y';
 
-while (true) {
-  if (userWinCounter === 3) {
-    prompt(`YOU: ${userWinCounter}\n=> CPU: ${computerWinCounter}\n=> You are the grand winner!`);
-    break;
-  } else if (computerWinCounter === 3) {
-    prompt(`YOU: ${userWinCounter}\n=> CPU: ${computerWinCounter}\n+> CPU is the grand winner!`);
-    break;
-  }
+while (playAgainAnswer === 'y') {
+  while (true) {
+    if (userWinCounter === 3) {
+      prompt(`YOU: ${userWinCounter}\n=> CPU: ${computerWinCounter}\n=> You are the grand winner!`);
+      break;
+    } else if (computerWinCounter === 3) {
+      prompt(`YOU: ${userWinCounter}\n=> CPU: ${computerWinCounter}\n+> CPU is the grand winner!`);
+      break;
+    }
 
-  prompt(`The score is\n=> YOU: ${userWinCounter}\n=> CPU: ${computerWinCounter}`);
+    prompt(`The score is\n=> YOU: ${userWinCounter}\n=> CPU: ${computerWinCounter}`);
 
-  prompt(`Choose one: ${VALID_CHOICES.join(', ')}`);
-  let choice = readline.question().toLowerCase();
-
-  switch (choice) {
-    case 'r':
-      choice = 'rock';
-      break;
-    case 'p':
-      choice = 'paper';
-      break;
-    case 'sc':
-      choice = 'scissors';
-      break;
-    case 'l':
-      choice = 'lizard';
-      break;
-    case 'sp':
-      choice = 'spock';
-      break;
-  }
-
-  while (!VALID_CHOICES.includes(choice)) {
-    prompt(`That's not a valid choice.`);
-    choice = readline.question().toLowerCase();
+    prompt(`Choose one: ${VALID_CHOICES.join(', ')} (r, p, sc, l, sp).`);
+    let choice = readline.question().toLowerCase();
 
     switch (choice) {
       case 'r':
@@ -86,18 +66,58 @@ while (true) {
         choice = 'spock';
         break;
     }
+
+    while (!VALID_CHOICES.includes(choice)) {
+      prompt(`That's not a valid choice.`);
+      choice = readline.question().toLowerCase();
+
+      switch (choice) {
+        case 'r':
+          choice = 'rock';
+          break;
+        case 'p':
+          choice = 'paper';
+          break;
+        case 'sc':
+          choice = 'scissors';
+          break;
+        case 'l':
+          choice = 'lizard';
+          break;
+        case 'sp':
+          choice = 'spock';
+          break;
+      }
+    }
+
+    let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
+    let computerChoice = VALID_CHOICES[randomIndex];
+
+    console.clear();
+
+    prompt(`You chose ${choice}; CPU chose ${computerChoice}.`);
+
+    prompt(displayWinner(choice, computerChoice));
+
+    if (displayWinner(choice, computerChoice) === 'You won!') {
+      userWinCounter += 1;
+    } else if (displayWinner(choice, computerChoice) === 'CPU won!') {
+      computerWinCounter += 1;
+    }
   }
 
-  let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
-  let computerChoice = VALID_CHOICES[randomIndex];
+  prompt('Would you like to play again? y/n');
+  playAgainAnswer = readline.question();
+  playAgainAnswer = playAgainAnswer[0].toLowerCase();
 
-  prompt(`You chose ${choice}; CPU chose ${computerChoice}.`);
+  while (playAgainAnswer !== 'y' && playAgainAnswer !== 'n') {
+    prompt('Invalid choice. Please enter y or n.');
+    playAgainAnswer = readline.question();
+    playAgainAnswer = playAgainAnswer[0].toLowerCase();
+  }
 
-  prompt(displayWinner(choice, computerChoice));
-
-  if (displayWinner(choice, computerChoice) === 'You won!') {
-    userWinCounter += 1;
-  } else if (displayWinner(choice, computerChoice) === 'CPU won!') {
-    computerWinCounter += 1;
+  if (playAgainAnswer === 'y') {
+    userWinCounter = 0;
+    computerWinCounter = 0;
   }
 }
